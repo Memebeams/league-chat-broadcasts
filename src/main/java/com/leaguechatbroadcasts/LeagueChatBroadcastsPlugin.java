4580ae5497c86fd6e299d5fb21b33ef933fca225
Gerhard Tetzner <tetzner.gerhard@gmail.com>
2022-02-05 17:30:28 -0500

    Fix plugin not working for recipients with spaces in their name

--- src/main/java/com/leaguechatbroadcasts/LeagueChatBroadcastsPlugin.java
+++ src/main/java/com/leaguechatbroadcasts/LeagueChatBroadcastsPlugin.java
@@ -1,7 +1,5 @@
 package com.leaguechatbroadcasts;
 
-import com.google.common.collect.ImmutableList;
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
@@ -9,9 +7,7 @@ import net.runelite.api.clan.ClanChannel;
 import net.runelite.api.clan.ClanChannelMember;
 import net.runelite.api.clan.ClanID;
 import net.runelite.api.events.*;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
-import net.runelite.client.events.WorldsFetch;
 import net.runelite.client.game.WorldService;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -29,7 +25,7 @@ import java.util.stream.Collectors;
 )
 public class LeagueChatBroadcastsPlugin extends Plugin
 {
-	private final Map<String, Set<String>> CLAN_NAMES = new HashMap<>();
+	private final Map<String, Set<String>> CLAN_MEMBER_NAMES = new HashMap<>();
 	private Set<Integer> LEAGUE_WORLDS = new HashSet<>();
 
 	@Inject
@@ -70,27 +66,27 @@ public class LeagueChatBroadcastsPlugin extends Plugin
 		List<ClanChannel> channels = getClanChannels().stream().filter(Objects::nonNull).collect(Collectors.toList());
 
 		List<String> channelNames = channels.stream().map(ClanChannel::getName).collect(Collectors.toList());
-		for (String key : CLAN_NAMES.keySet()) {
-			if (!channelNames.contains(key)) CLAN_NAMES.remove(key);
+		for (String key : CLAN_MEMBER_NAMES.keySet()) {
+			if (!channelNames.contains(key)) CLAN_MEMBER_NAMES.remove(key);
 		}
 
 		for (ClanChannel channel : channels) {
-			CLAN_NAMES.put(channel.getName(), getNamesOfMembers(channel));
+			CLAN_MEMBER_NAMES.put(channel.getName(), getNamesOfMembers(channel));
 		}
 	}
 
 	@Subscribe
 	public void onClanMemberJoined(ClanMemberJoined event) {
 		String channelName = event.getClanChannel().getName();
-		if (!CLAN_NAMES.containsKey(channelName)) return;
-		CLAN_NAMES.get(channelName).add(event.getClanMember().getName());
+		if (!CLAN_MEMBER_NAMES.containsKey(channelName)) return;
+		CLAN_MEMBER_NAMES.get(channelName).add(event.getClanMember().getName());
 	}
 
 	@Subscribe
 	public void onClanMemberLeft(ClanMemberLeft event) {
 		String channelName = event.getClanChannel().getName();
-		if (!CLAN_NAMES.containsKey(channelName)) return;
-		CLAN_NAMES.get(channelName).remove(event.getClanMember().getName());
+		if (!CLAN_MEMBER_NAMES.containsKey(channelName)) return;
+		CLAN_MEMBER_NAMES.get(channelName).remove(event.getClanMember().getName());
 	}
 
 	@Subscribe
@@ -107,24 +103,32 @@ public class LeagueChatBroadcastsPlugin extends Plugin
 		// All loot drops we care about have a few words followed by a colon followed by the drop
 		Pattern expectedFormat = Pattern.compile("[\\w\\s]+:.*");
 
-		String message = event.getMessage();
+		String message = Text.sanitize(event.getMessage());
 		Matcher regex = expectedFormat.matcher(message);
+
 		if (!regex.matches()) return;
 
-		String playerName = event.getMessage().substring(0, message.indexOf(" "));
+		Set<String> allNames = new HashSet<>();
+		CLAN_MEMBER_NAMES.values().forEach(allNames::addAll);
 
-		CLAN_NAMES.entrySet().stream().filter(entry -> entry.getValue().contains(playerName)).findFirst().ifPresent(entry -> {
-			String clanName = entry.getKey();
+		Optional<String> possibleClanmateName = allNames.stream().filter(message::startsWith).findAny();
+		if (!possibleClanmateName.isPresent()) return;
+		String clanmateName = possibleClanmateName.get();
 
-			ClanChannel channel = getClanChannels().stream().filter(clanChannel -> clanChannel.getName().equals(clanName)).findFirst().orElse(null);
-			if (channel == null) return;
+		Optional<String> possibleClanName = CLAN_MEMBER_NAMES.entrySet().stream().filter(entry -> entry.getValue().contains(clanmateName)).map(Map.Entry::getKey).findAny();
+		if (!possibleClanName.isPresent()) return;
+		String clanName = possibleClanName.get();
 
-			ClanChannelMember clanmate = channel.getMembers().stream().filter(member -> member.getName().equals(playerName)).findFirst().orElse(null);
-			if (clanmate == null) return;
+		Optional<ClanChannel> possibleClan = getClanChannels().stream().filter(clanChannel -> clanChannel.getName().equals(clanName)).findAny();
+		if (!possibleClan.isPresent()) return;
+		ClanChannel clan = possibleClan.get();
 
-			if (LEAGUE_WORLDS.contains(clanmate.getWorld())) {
-				event.getMessageNode().setValue(IconID.LEAGUE + event.getMessage());
-			}
-		});
+		Optional<ClanChannelMember> possibleClanMember = clan.getMembers().stream().filter(clanMember -> clanMember.getName().equals(clanmateName)).findAny();
+		if (!possibleClanMember.isPresent()) return;
+		ClanChannelMember clanMember = possibleClanMember.get();
+
+		if (LEAGUE_WORLDS.contains(clanMember.getWorld())) {
+			event.getMessageNode().setValue(IconID.LEAGUE + event.getMessage());
+		}
 	}
 }
